22edf7db4352633e7cfaa9a62f85dc6621157db8
Shani gunasekara <83491210+shani997@users.noreply.github.com>
2024-06-26 20:07:51 +0530

    Create crud

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express"; 
+import mongoose from "mongoose"; 
+import bodyParser from "body-parser"; 
+import dotenv from "dotenv"; 
+import route from "./routes/userRoutes.js";
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5000;
+
+
+const MONGOURL = process.env.MONGO_URL;
+
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Database connected successfully."); 
+  
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`); 
+    });
+  })
+  .catch((error) => console.log(error)); 
+
+  app.use("/api/user", route);
